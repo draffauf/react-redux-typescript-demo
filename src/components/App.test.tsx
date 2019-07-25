1a255e4f6811e6cd50055ce9aa632bb9793f4e97
David Raffauf <services@davidraffauf.com>
2019-07-25 10:35:30 -0700

    Add Enzyme, update test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  shallow(<App />);
+describe('App', () => {
+  const wrapper = shallow(<App />);
+
+  it('renders a heading', () => {
+    const element = <h1>The Force Awakens</h1>;
+    expect(wrapper.contains(element)).toEqual(true);
+  }); 
 });
