ca3242097db325035ed6f717749452068bd998c2
David Raffauf <services@davidraffauf.com>
2019-07-25 14:25:28 -0700

    Improve the Root tests

--- src/components/root/Root.test.tsx
+++ src/components/root/Root.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import App from '../app/App';
 import Root from './Root';
+import configureStore from '../../store/Store';
 
 describe('Root', () => {
-  it('true is true', () => {
-    expect(true).toBe(true);
+  const store = configureStore();
+  const wrapper = shallow(<Root store={store} />);
+
+  describe('renders', () => {
+    it('App', () => {
+      const element = <App />;
+      expect(wrapper.contains(element)).toEqual(true);
+    });  
+  });
+
+  describe('properties', () => {
+    it('store', () => expect(wrapper.props().store).toEqual(store));  
   });
 });
