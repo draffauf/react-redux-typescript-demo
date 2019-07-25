0c331da47be00ffe12d0a73f214f8579b02bf831
David Raffauf <services@davidraffauf.com>
2019-07-25 14:28:50 -0700

    Add test to prove App renders CharacterList

--- src/components/app/App.test.tsx
+++ src/components/app/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import CharacterList from '../../containers/CharacterList';
 
 describe('App', () => {
   const wrapper = shallow(<App />);
@@ -10,5 +11,10 @@ describe('App', () => {
       const element = <h1>The Force Awakens</h1>;
       expect(wrapper.contains(element)).toEqual(true);
     });  
+
+    it('CharacterList', () => {
+      const element = <CharacterList />;
+      expect(wrapper.contains(element)).toEqual(true);
+    });
   });
 });
