d1016d3f49eb7411b07125bcfe3551245316f0dc
David Raffauf <services@davidraffauf.com>
2019-07-27 14:53:36 -0700

    Improve character reducer test coverage

--- src/character/characterReducer.test.ts
+++ src/character/characterReducer.test.ts
@@ -1,36 +1,60 @@
 import characterReducer from './characterReducer';
 import ICharacterState from "./ICharacterState.interface";
 import {
+  IGetCharactersStartAction,
   IGetCharactersSuccessAction,
   IGetCharactersFailureAction
 } from "./IGetCharactersActions.interface";
 import CharacterActionTypes from './CharacterActionTypes.enum';
 import getCharactersMock from './getCharactersMock';
 
-describe('characterReducer', () => {
-  describe('when successful', () => {
+const initialState: ICharacterState = {
+  characters: [],
+  isFetching: false,
+};
+
+describe('characterReducer action responses for', () => {
+  describe('GET_CHARACTERS_START', () => {
+    const action: IGetCharactersStartAction = {
+      type: CharacterActionTypes.GET_CHARACTERS_START,
+      isFetching: true,
+    };
+
+    const newState = characterReducer(initialState, action);
+    it('is fetching', () => {
+      expect(newState.isFetching).toBe(true);
+    });
+  });
+
+  describe('GET_CHARACTERS_SUCCESS', () => {
     const action: IGetCharactersSuccessAction = {
       type: CharacterActionTypes.GET_CHARACTERS_SUCCESS,
       characters: getCharactersMock,
+      isFetching: false,
     };
-    const initialState: ICharacterState = {
-      characters: [],
-    };
-    const newState = characterReducer(initialState, action)
-      it('returns new state with fetched characters', () => {
-        expect(newState.characters).toEqual(getCharactersMock);
-      });
+
+    const newState = characterReducer(initialState, action);
+    it('has fetched characters', () => {
+      expect(newState.characters).toEqual(getCharactersMock);
+    });
+
+    it('is not fetching', () => {
+      expect(newState.isFetching).toBe(false);
+    });
   });
 
-  describe('default path for unknown type', () => {
-    const fakeAction: IGetCharactersFailureAction = {
-      type: CharacterActionTypes.GET_CHARACTERS_FAILURE
+  describe('GET_CHARACTERS_FAILURE', () => {
+    const action: IGetCharactersFailureAction = {
+      type: CharacterActionTypes.GET_CHARACTERS_FAILURE,
+      isFetching: false,
     };
-    const initialState: ICharacterState = {
-      characters: [],
-    };
-    const newState = characterReducer(initialState, fakeAction)
 
-    it('returns the same state', () => expect(newState).toEqual(initialState));
+    const newState = characterReducer(initialState, action);
+
+    it('does not have fetched characters', () => {
+      expect(newState.characters).toEqual([]);
+    });
+
+    it('is not fetching', () => expect(newState.isFetching).toBe(false));
   });
 });
